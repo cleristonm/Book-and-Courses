2e2974c51a0d658743044215602fcf493dec11c3
Cleriston Martinelo <cleriston@gmail.com>
2020-09-05 20:21:42 -0300

    Creating threads using a factory

---
+++ Java-Concurrency-Cookbook/10-Creating threads through a factory/src/com/keko/code/Task.java
@@ -0,0 +1,17 @@
+package com.keko.code;
+
+import java.util.concurrent.TimeUnit;
+
+public class Task implements Runnable {
+
+	@Override
+	public void run() {
+		try {
+			TimeUnit.SECONDS.sleep(1);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
