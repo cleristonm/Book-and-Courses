9c23a6fbdc217876458a598f7c63f66896c90ef8
Cleriston Martinelo <cleriston@gmail.com>
2020-09-10 22:03:13 -0300

    Thread executors

---
+++ Java-Concurrency-Cookbook/24-Creating a thread executor and controlling its rejected tasks/src/com/keko/code/Main.java
@@ -0,0 +1,21 @@
+package com.keko.code;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Server server = new Server();
+		
+		System.out.println("Main: Starting");
+		for (int i=0; i<100; i++) {
+			Task task = new Task("Task "+i);
+			server.executeTask(task);
+		}
+		System.out.printf("Main: Shuting down the Executor.\n");
+		server.endServer();
+		System.out.printf("Main: Sending another Task.\n");
+		Task task=new Task("Rejected task");
+		server.executeTask(task);
+		System.out.printf("Main: End.\n");
+	}
+
+}
---
+++ Java-Concurrency-Cookbook/26-Running multiple tasks and processing the first result/src/com/keko/code/ValidatorTask.java
@@ -0,0 +1,35 @@
+package com.keko.code;
+
+import java.util.concurrent.Callable;
+
+public class ValidatorTask implements Callable<String> {
+	
+	private final UserValidator validator;
+	private final String user;
+	private final String password;
+	
+	
+	
+	public ValidatorTask(UserValidator validator, String user, String password) {
+		super();
+		this.validator = validator;
+		this.user = user;
+		this.password = password;
+	}
+
+
+	@Override
+	public String call() throws Exception {
+		if (!validator.validate(user, password)) {
+			System.out.printf("%s: The user has not been found\n",
+			validator.getName());
+			throw new Exception("Error validating user");
+		}
+		System.out.printf("%s: The user has been found\n",
+				validator.getName());
+		return validator.getName();
+		
+		
+	}
+
+}
